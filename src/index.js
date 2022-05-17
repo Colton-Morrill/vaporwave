c72225237fa4a49b58e1fe6e804e280b0f6650d9
coltonmorrill <colton@tacksystems.com>
2022-05-17 16:47:06 -0600

    General updates

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "../styles.css";
-import * as THREE from "three";
+import * as THREE from "../node_modules/three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js";
 import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js";
