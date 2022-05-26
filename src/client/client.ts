fd4cb890f4687ee5cd601803fcf2228aeca3ee07
coltonmorrill <colton@tacksystems.com>
2022-05-26 16:47:10 -0600

    Performance and render updates.

--- src/client/client.ts
+++ src/client/client.ts
@@ -7,10 +7,12 @@ import { GammaCorrectionShader } from "three/examples/jsm/shaders/GammaCorrectio
 import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass.js";
 import { RGBShiftShader } from "three/examples/jsm/shaders/RGBShiftShader.js";
 import * as jQuery from 'jquery';
+import {FBXLoader} from 'three/examples/jsm/loaders/FBXLoader';
+import { sRGBEncoding } from "three";
 
-const TEXTURE_PATH = "https://res.cloudinary.com/dg5nsedzw/image/upload/v1641657168/blog/vaporwave-threejs-textures/grid.png";
-const DISPLACEMENT_PATH = "https://res.cloudinary.com/dg5nsedzw/image/upload/v1641657200/blog/vaporwave-threejs-textures/displacement.png";
-const METALNESS_PATH = "https://res.cloudinary.com/dg5nsedzw/image/upload/v1641657200/blog/vaporwave-threejs-textures/metalness.png";
+const TEXTURE_PATH = "assets/textures/grid.png";
+const DISPLACEMENT_PATH = "assets/textures/displacement.png";
+const METALNESS_PATH = "assets/textures/metalness.png";
 var screenWidth = window.innerWidth;
 // Textures
 const textureLoader = new THREE.TextureLoader();
@@ -24,23 +26,25 @@ const scene = new THREE.Scene();
 
 //Load background texture
 const loader = new THREE.TextureLoader();
-loader.load('assets/images/newpic.jpg' , function(texture)
-            {
-             scene.background = texture;  
-             var repeatX, repeatY;
-             var clothWidth = window.innerWidth;
-             var clothHeight = window.innerHeight;
-             var textureSettingh = 406;
-             var textureSettingw = 423;
-             texture.wrapS = THREE.ClampToEdgeWrapping;
-             texture.wrapT = THREE.RepeatWrapping;
-            repeatX = clothWidth * textureSettingh / (clothHeight * textureSettingw);
-            repeatY = 1;
-            texture.repeat.set(repeatX, repeatY);
-            texture.offset.x = (repeatX - 1) / 2 * -1;
-            });
+loader.load('assets/images/newpic.jpg', function (texture) {
+    texture.encoding = THREE.sRGBEncoding;
+    scene.background = texture;
+    var repeatX, repeatY;
+    var clothWidth = window.innerWidth;
+    var clothHeight = window.innerHeight;
+    console.log(clothWidth, clothHeight);
+    var textureSettingh = 4500;
+    var textureSettingw = 9992;
+    texture.wrapS = THREE.ClampToEdgeWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    repeatX = clothWidth * textureSettingh / (clothHeight * textureSettingw);
+    console.log(repeatX);
+    repeatY = 1;
+    texture.repeat.set(repeatX, repeatY);
+    texture.offset.x = (repeatX - 1) / 2 * -1;
+});
 
-            
+
 
 // Fog
 const fog = new THREE.Fog("#000000", 1, 2.5);
@@ -62,47 +66,71 @@ const material = new THREE.MeshStandardMaterial({
     /**
      * Add a metalnessMap to our material that will tell the renderer
      * where the "rough" parts of our terrains are
-     */ 
+     */
     metalnessMap: metalnessTexture,
     /**
      * Make the terrain very very metallic so it will reflect the light
      * and not diffuse it: it will stay black
-     */ 
+     */
     metalness: 0.96,
     /**
      * Make the terrain a bit rough so the rough parts will diffuse the light
      * well
-     */ 
+     */
     roughness: 0.5,
 });
-const boxArt = new THREE.TextureLoader().load('boxart.png')
-const geometry2 = new THREE.BoxGeometry( 1, 1, 1 );
-const material2 = new THREE.MeshBasicMaterial( {map: boxArt,} );
-const cube = new THREE.Mesh( geometry2, material2 );
-const cube2 = new THREE.Mesh( geometry2, material2 );
+
+
+var workBox = textureLoader.load('front-box.png');
+workBox.encoding = sRGBEncoding;
+var sideBox = textureLoader.load('assets/images/side-box.png');
+sideBox.encoding = sRGBEncoding;
+var frontAbout = textureLoader.load('assets/images/front-about.png');
+frontAbout.encoding = sRGBEncoding;
+
+const geometry2 = new THREE.BoxGeometry(1, 1, 1);
+const cubeMaterials = [
+    new THREE.MeshBasicMaterial({ map: sideBox}), //right side
+    new THREE.MeshBasicMaterial({ map: sideBox}), //left side
+    new THREE.MeshBasicMaterial({color: 0x000000}), //top side
+    new THREE.MeshBasicMaterial({color: 0x000000}), //bottom side
+    new THREE.MeshBasicMaterial({map: workBox}), //front side
+    new THREE.MeshBasicMaterial({ map: sideBox}), //back side
+];
+const cubeMaterialsAbout = [
+    new THREE.MeshBasicMaterial({ map: sideBox}), //right side
+    new THREE.MeshBasicMaterial({ map: sideBox}), //left side
+    new THREE.MeshBasicMaterial({color: 0x000000}), //top side
+    new THREE.MeshBasicMaterial({color: 0x000000}), //bottom side
+    new THREE.MeshBasicMaterial({map: frontAbout}), //front side
+    new THREE.MeshBasicMaterial({ map: sideBox}), 
+];
+ 
+const cube = new THREE.Mesh(geometry2, cubeMaterials);
+const cube2 = new THREE.Mesh(geometry2, cubeMaterialsAbout);
 
 var width = 0.125,
-        height = 0.15,
-        length = 0.025;
+    height = 0.15,
+    length = 0.025;
 
-    cube.scale.x = width;
-    cube.scale.y = height;
-    cube.scale.z = length;
-    cube.position.y = 0.2;
-    cube.position.z = 0.5;
-    cube.rotation.y = 0.35;
-    cube.rotation.x = 0.25;
-    cube.name = 'middle';
+cube.scale.x = width;
+cube.scale.y = height;
+cube.scale.z = length;
+cube.position.y = 0.1;
+cube.position.z = 0.5;
+cube.rotation.y = 0.35;
+cube.rotation.x = 0.25;
+cube.name = 'middle';
 
-    cube2.scale.x = width;
-    cube2.scale.y = height;
-    cube2.scale.z = length;
-    cube2.position.y = 0.2;
-    cube2.position.z = 0.5;
-    cube2.position.x = 0.25;
-    cube2.rotation.y = 0.15;
-    cube2.rotation.x = -0.05;
-    cube2.name = "right";
+cube2.scale.x = width;
+cube2.scale.y = height;
+cube2.scale.z = length;
+cube2.position.y = 0.1;
+cube2.position.z = 0.5;
+cube2.position.x = 0.25;
+cube2.rotation.y = 0.15;
+cube2.rotation.x = -0.05;
+cube2.name = "right";
 
 
 if (screenWidth <= 950) {
@@ -113,6 +141,34 @@ else {
     scene.add(cube);
     scene.add(cube2);
 }
+
+// const fbxLoader = new FBXLoader();
+// fbxLoader.load(
+//     'assets/models/nes.fbx',
+    
+//     (nes) => {  nes.traverse(function (child) {
+//             if ((child as THREE.Mesh).isMesh) {
+//                 (child as THREE.Mesh).material = nesMaterial
+//                 if ((child as THREE.Mesh).material) {
+//                     ((child as THREE.Mesh).material as THREE.MeshBasicMaterial).transparent = false
+//                 }
+//             }
+//         })
+//         var width = 0.125,
+//         height = 0.15,
+//         length = 0.025;
+
+//         nes.scale.x = width;
+//         nes.scale.y = height;
+//         nes.scale.z = length;
+//         nes.position.y = 0.1;
+//         nes.position.z = 0.5;
+//         nes.rotation.y = 0.35;
+//         nes.rotation.x = 0.25;
+//         nes.name = 'middle';
+//         nes.scale.set(.01, .01, .01)
+//         scene.add(nes)}
+// )
 
 const plane = new THREE.Mesh(geometry, material);
 plane.rotation.x = -Math.PI * 0.5;
@@ -130,7 +186,7 @@ scene.add(plane2);
 
 // Light
 // Ambient Light
-const ambientLight = new THREE.AmbientLight("#ffffff", 10);
+const ambientLight = new THREE.AmbientLight("#800fff", 10);
 scene.add(ambientLight);
 
 
@@ -157,33 +213,34 @@ scene.add(spotlight2.target);
 
 // Sizes
 const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
+    width: window.innerWidth,
+    height: window.innerHeight,
 };
 
 // Camera
 const camera = new THREE.PerspectiveCamera(
-  75,
-  sizes.width / sizes.height,
-  0.01,
-  20
+    75,
+    sizes.width / sizes.height,
+    0.01,
+    20
 );
 camera.position.x = 0;
 camera.position.y = 0.06;
 camera.position.z = 1.1;
 
 // Controls
-const controls = new OrbitControls(camera,canvas);
+const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 controls.enabled = false;
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
+    canvas: canvas,
+    
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
+renderer.outputEncoding = THREE.sRGBEncoding;
 // Post Processing
 const effectComposer = new EffectComposer(renderer);
 effectComposer.setSize(sizes.width, sizes.height);
@@ -209,6 +266,7 @@ window.addEventListener("resize", () => {
     // Update renderer
     renderer.setSize(sizes.width, sizes.height);
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  
 
     // Update effect composer
     effectComposer.setSize(sizes.width, sizes.height);
@@ -259,28 +317,27 @@ else {
 }
 
 function onDocumentMouseGrow(event: MouseEvent) {
-  raycaster.setFromCamera(
-      {
-          x: (event.clientX / renderer.domElement.clientWidth) * 2 - 1,
-          y: -(event.clientY / renderer.domElement.clientHeight) * 2 + 1
-      },
-      camera
-  )
-  intersects = raycaster.intersectObjects(pickableObjects, false)
-  var frame = 0.5;
-  if (intersects.length > 0) {
-      var width = 0.25,
-          height = 0.3,
-          length = 0.05;
-      intersectedObject = intersects[0].object
-      intersectedObject.scale.x = width;
-      intersectedObject.scale.y = height;
-      intersectedObject.scale.z =length;
-      jQuery('html').css('cursor', 'pointer');
-  } else  {
-    var width = 0.125,
-        height = 0.15,
-        length = 0.025;
+    raycaster.setFromCamera(
+        {
+            x: (event.clientX / renderer.domElement.clientWidth) * 2 - 1,
+            y: -(event.clientY / renderer.domElement.clientHeight) * 2 + 1
+        },
+        camera
+    )
+    intersects = raycaster.intersectObjects(pickableObjects, false)
+    if (intersects.length > 0) {
+        var width = 0.15,
+            height = 0.2,
+            length = 0.03;
+        intersectedObject = intersects[0].object
+        intersectedObject.scale.x = width;
+        intersectedObject.scale.y = height;
+        intersectedObject.scale.z = length;
+        jQuery('html').css('cursor', 'pointer');
+    } else {
+        var width = 0.125,
+            height = 0.15,
+            length = 0.025;
         intersectedObject = null
         pickableObjects[0].scale.x = width;
         pickableObjects[0].scale.y = height;
@@ -289,7 +346,7 @@ function onDocumentMouseGrow(event: MouseEvent) {
         pickableObjects[1].scale.y = height;
         pickableObjects[1].scale.z = length;
         jQuery('html').css('cursor', 'auto');
-  }
+    }
 }
 function onDocumentMouseMove(event: MouseEvent) {
     raycaster.setFromCamera(
@@ -302,9 +359,24 @@ function onDocumentMouseMove(event: MouseEvent) {
     intersects = raycaster.intersectObjects(pickableObjects, false)
 
     if (intersects.length > 0) {
-        console.log('working')
-        var element = document.getElementsByClassName('popup')[0];
-        element.classList.remove('d-none');
+        intersectedObject = intersects[0].object
+        var selectedBox = intersectedObject.name;
+        switch (selectedBox) {
+            case 'left':
+                var element = document.getElementsByClassName('popup3')[0];
+                element.classList.remove('d-none');
+                break;
+            case 'middle':
+                var element = document.getElementsByClassName('popup')[0];
+                element.classList.remove('d-none');
+                break;
+            case 'right':
+                var element = document.getElementsByClassName('popup2')[0];
+                element.classList.remove('d-none');
+                break;
+        }
+        document.removeEventListener('mousedown', onDocumentMouseMove, false)
+        document.removeEventListener('mousemove', onDocumentMouseGrow, false)
 
     } else {
         intersectedObject = null
@@ -316,24 +388,26 @@ function onWindowResize() {
 
 
     const loader = new THREE.TextureLoader();
-    loader.load('assets/images/newpic.jpg' , function(texture)
-            {
-            scene.background = texture;  
-            var repeatX, repeatY;
-            var clothWidth = window.innerWidth;
-            var clothHeight = window.innerHeight;
-            var textureSettingh = 406;
-            var textureSettingw = 423;
-            texture.wrapS = THREE.ClampToEdgeWrapping;
-            texture.wrapT = THREE.RepeatWrapping;
-            repeatX = clothWidth * textureSettingh / (clothHeight * textureSettingw);
-            repeatY = 1;
-            texture.repeat.set(repeatX, repeatY);
-            texture.offset.x = (repeatX - 1) / 2 * -1;
+    loader.load('assets/images/newpic.jpg', function (texture) {
+        scene.background = texture;
+        var repeatX, repeatY;
+        var clothWidth = window.innerWidth;
+        var clothHeight = window.innerHeight;
+        var textureSettingh = 406;
+        var textureSettingw = 423;
+        texture.wrapS = THREE.ClampToEdgeWrapping;
+        texture.wrapT = THREE.RepeatWrapping;
+        repeatX = clothWidth * textureSettingh / (clothHeight * textureSettingw);
+        repeatY = 1;
+        texture.repeat.set(repeatX, repeatY);
+        texture.offset.x = (repeatX - 1) / 2 * -1;
     });
 }
 
+jQuery( ".close-button" ).click(function() {
+    jQuery('.window').addClass('d-none');
+    document.addEventListener('mousedown', onDocumentMouseMove, false)
+    document.addEventListener('mousemove', onDocumentMouseGrow, false)
+  });
 
- 
 
-           
