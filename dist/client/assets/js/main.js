340dbabfbb2d16c5038867ecea8883f3b9cca9a9
coltonmorrill <colton@tacksystems.com>
2022-06-20 15:24:38 -0600

    Adds mobile page.

--- dist/client/assets/js/main.js
+++ dist/client/assets/js/main.js
@@ -10,3 +10,13 @@ $( ".img-cont" ).click(function() {
 $( ".lightbox-close" ).click(function() {
     $('.lightbox').addClass('d-none');
 });
+
+$( ".mobile-option.personal" ).click(function() {
+    $('.popup3').removeClass('d-none');
+});
+$( ".mobile-option.work" ).click(function() {
+    $('.popup').removeClass('d-none');
+});
+$( ".mobile-option.about" ).click(function() {
+    $('.popup2').removeClass('d-none');
+});
